6ebe67f0b5997eae2b22d5103d047a63f23b77da
John Datserakis <johndatserakis@gmail.com>
2019-08-31 23:32:29 -0400

    Updated margin on lead to fix animation issue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -383,12 +383,13 @@ export default {
 
     &__lead-text {
       margin-bottom: 20px;
-      margin-left: 4px;
+      margin-left: 6px;
       font-size: 1.8rem;
       font-weight: bold;
       color: $black;
 
       @media (min-width: 992px) {
+        margin-left: 0px;
         font-size: 4rem;
       }
     }
